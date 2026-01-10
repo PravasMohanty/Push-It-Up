10a0d9f43349b67e655071f4cf308e8173a3a0f6
Akash <akpra7173@gmail.com>
2026-01-10 12:04:54 +0530

    Fix:  First_Year/C -changed the condition to i < size so that array does not go out of bounds

--- First_Year/C/bug_02_array_sum.c
+++ First_Year/C/bug_02_array_sum.c
@@ -7,7 +7,7 @@
 int sum_array(int arr[], int size)
 {
     int sum = 0;
-    for (int i = 0; i <= size; i++)  
+    for (int i = 0; i < size; i++)  //Fixed: changed the condition from i <= size to i < size so that array does not go out of bounds
     {
         sum += arr[i];
     }
